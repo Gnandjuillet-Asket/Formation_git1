1e8f83e0873d8bebde720cf3b894f561bc121911
Gnandjuillet Asket <asketdegni@gmail.com>
2025-03-28 23:03:53 +0000

    Update index.py
    modification du fichier index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 print("Dieu est Bon, car il nous a envoyer son fils unique pourque quiconque crois soit sauver.")
 
 age = 18;
-nom = input("Entrez vous Nom")
+nom = input("Entrez vous Nom: ")
 utilisateur = int(input("Veillez entrez vous âge: "))
 if age <= utilisateur:
   print(f"Bonjour {nom} vous êtes majeur !")
